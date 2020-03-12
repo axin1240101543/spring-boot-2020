e6e71a57b08b21a9e46d865aef443d2aa04ed08a
Darren <1240101543@qq.com>
2020-03-12 22:00:57 +0800

    finalize method +1

--- spring-boot-utils/src/main/java/com/darren/center/springboot/gc/Finalization.java
+++ spring-boot-utils/src/main/java/com/darren/center/springboot/gc/Finalization.java
@@ -1,4 +1,33 @@
 package com.darren.center.springboot.gc;
 
 public class Finalization {
+
+    public static Finalization finalization;
+
+    /**
+     * gc前会执行此方法
+     * @throws Throwable
+     */
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("finalized");
+        //回收此对象的时候，将这个对象的值再一次赋给了它的成员变量。
+        finalization = this;
+    }
+
+    public static void main(String[] args) {
+        Finalization f = new Finalization();
+        System.out.println("first print: " + f);
+        f = null;
+        //GC
+        System.gc();
+        //休息一段时间，让上面的垃圾回收线程执行完成，因为当主线程执行完成后，将会打断finalize方法的执行
+        try {
+            Thread.currentThread().sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("second print：" + f);
+        System.out.println(f.finalization);
+    }
 }
