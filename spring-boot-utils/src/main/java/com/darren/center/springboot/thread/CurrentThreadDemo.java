1cc27b0c63f9796661b6cb653e0335dce68036c7
Darren <1240101543@qq.com>
2020-03-16 21:51:32 +0800

    单线程编程模型

---
+++ spring-boot-utils/src/main/java/com/darren/center/springboot/thread/CurrentThreadDemo.java
@@ -0,0 +1,9 @@
+package com.darren.center.springboot.thread;
+
+public class CurrentThreadDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Current Thread: " + Thread.currentThread().getName());
+    }
+
+}
