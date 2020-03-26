9d7279522b3648d905afc86f0da50a0529d860b0
Darren <1240101543@qq.com>
2020-03-26 21:26:12 +0800

    锁消除

---
+++ spring-boot-utils/src/main/java/com/darren/center/springboot/thread/StringBufferWithoutSync.java
@@ -0,0 +1,19 @@
+package com.darren.center.springboot.thread;
+
+public class StringBufferWithoutSync {
+
+    public void add(String str1, String str2){
+        //StringBuffer是线程安全的，由于sb只会在append方法中使用，不可能被其他线程引用
+        //因此sb属于不可能共享的资源，JVM会自动消除内部锁
+        StringBuffer sb = new StringBuffer();
+        sb.append(str1).append(str2);
+    }
+
+    public static void main(String[] args) {
+        StringBufferWithoutSync withoutSync = new StringBufferWithoutSync();
+        for (int i = 0; i < 1000; i++) {
+            withoutSync.add("aaa", "bbb");
+        }
+    }
+
+}
